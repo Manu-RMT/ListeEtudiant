c2da6d8b5471a55c063bb417e57d3fc11231cfc7
Manu-RMT <mandu543@gmail.com>
2024-02-12 09:58:42 +0100

    init data étudiants fini

--- ListeEtudiant/ViewController.swift
+++ ListeEtudiant/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITabBarDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    var tableauEtudiant : [Etudiant] = []
+    @IBOutlet weak var listeEtudiant: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        creationTableauEtudiant()
+        
     }
-
+    
+    
+    // crée les etudiant
+    func creationTableauEtudiant() {
+        tableauEtudiant.append(Etudiant(nom: "RMT", prenom: "Manu", formation: "M2", groupeTP: "GRP 2", nomPhoto: "photoEtudiant.png", numAbsence: 0))
+        tableauEtudiant.append(Etudiant(nom: "MTL", prenom: "Quentin", formation: "M2", groupeTP: "GRP 2", nomPhoto: "photoEtudiant.png", numAbsence: 0))
+        tableauEtudiant.append(Etudiant(nom: "CHK", prenom: "Feriel", formation: "M2", groupeTP: "GRP 2", nomPhoto: "photoEtudiant.png", numAbsence: 0))
+        tableauEtudiant.append(Etudiant(nom: "ETU3", prenom: "etu", formation: "M2", groupeTP: "GRP 2", nomPhoto: "photoEtudiant.png", numAbsence: 0))
+        tableauEtudiant.append(Etudiant(nom: "ETU4", prenom: "etu4", formation: "M2", groupeTP: "GRP 2", nomPhoto: "photoEtudiant.png", numAbsence: 0))
+        tableauEtudiant.append(Etudiant(nom: "ETU5", prenom: "etudiant5", formation: "M2", groupeTP: "GRP 2", nomPhoto: "photoEtudiant.png", numAbsence: 0))
+    }
 
 }
 
