34fe861416091c3bf3e42a2c6571340642cdb7ce
Manu-RMT <mandu543@gmail.com>
2024-02-12 08:18:08 +0100

    Initial Commit

---
+++ ListeEtudiant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListeEtudiant
+//
+//  Created by Manuel Ramanitra on 12/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
